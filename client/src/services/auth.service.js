72e9c4244d7e5224e6690ea1c23cbcc9f7f211c2
Alan Huang <alan.h@tc-gaming.com>
2023-07-04 15:04:24 +0800

    refactor: change class methods to static methods

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 const API_URL = 'http://localhost:8080/api/user'; //最基本的URL
 
+//JS的class可以創造很多物件
 class AuthService {
-  //JS的class可以創造很多物件//AuthService可以有很多methods
-  login(email, password) {
+  //AuthService可以有很多methods
+  static login(email, password) {
     return axios.post(API_URL + '/login', { email, password });
   }
-  logout() {
+
+  static logout() {
     localStorage.removeItem('user');
   }
-  register(username, email, password, role) {
+
+  static register(username, email, password, role) {
     return axios.post(API_URL + '/register', {
       username,
       email,
@@ -17,9 +20,11 @@ class AuthService {
       role,
     });
   }
-  getCurrentUser() {
+
+  //目前正在使用網頁的人是誰
+  static getCurrentUser() {
     return JSON.parse(localStorage.getItem('user'));
-  } //目前正在使用網頁的人是誰
+  }
 }
 
-export default new AuthService(); //因AuthService是一個class 這裡直接幫這個class做一個物件出來
+export default AuthService;
