8f8c81b06606bec1c804fc4f47a6cbff9f0a9929
godmmt <mingreal75@gmail.com>
2023-08-02 21:18:00 +0800

    feat: About page

---
+++ client/src/config/about.config.js
@@ -0,0 +1,31 @@
+import instructor1 from 'assets/images/instructor1.jpg';
+import instructor2 from 'assets/images/instructor2.jpg';
+import instructor3 from 'assets/images/instructor3.jpg';
+import instructor4 from 'assets/images/instructor4.jpg';
+
+export const instructorCards = [
+  {
+    username: 'John Aston',
+    category: 'Web Development Instructor',
+    imgSrc: instructor1,
+    imgAlt: 'instructor1',
+  },
+  {
+    username: 'Erika Taylor',
+    category: 'Mobile Development Instructor',
+    imgSrc: instructor2,
+    imgAlt: 'instructor2',
+  },
+  {
+    username: 'Amy Rose',
+    category: 'Machine Learning Instructor',
+    imgSrc: instructor3,
+    imgAlt: 'instructor3',
+  },
+  {
+    username: 'Adrian Cruz',
+    category: 'Data Science Instructor',
+    imgSrc: instructor4,
+    imgAlt: 'instructor4',
+  },
+];
