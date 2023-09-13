46944d93f63af4f486e153a9a00d2906c04d347e
godmmt <mingreal75@gmail.com>
2023-09-13 18:50:49 +0800

    feat: Private Routes 引入 Modal 並且設定可以控制背景布幕為不透明色

--- client/src/utils/PrivateRoutes.jsx
+++ client/src/utils/PrivateRoutes.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { createPortal } from 'react-dom';
+import { useState } from 'react';
+import Modal from 'components/Modal';
+import Login from 'components/Login';
+import Register from 'components/Register';
 
-const PrivateRoutes = ({ currentUser, setIsModalOpen }) => {
-  return currentUser ? <Outlet /> : setIsModalOpen(true);
+const PrivateRoutes = ({ currentUser, setCurrentUser, setIsModalOpen }) => {
+  const [hasAccount, setHasAccount] = useState(true);
+  const handleCloseLoginModal = () => {
+    setIsModalOpen(false);
+    setHasAccount(true);
+  };
+
+  return currentUser ? (
+    <Outlet />
+  ) : (
+    createPortal(
+      <Modal onClose={handleCloseLoginModal} isCovered>
+        {hasAccount ? (
+          <Login setCurrentUser={setCurrentUser} handleCloseLoginModal={handleCloseLoginModal} setHasAccount={setHasAccount} hideCloseIcon />
+        ) : (
+          <Register setHasAccount={setHasAccount} handleCloseLoginModal={handleCloseLoginModal} hideCloseIcon />
+        )}
+      </Modal>,
+      document.body
+    )
+  );
 };
 
 export default PrivateRoutes;
