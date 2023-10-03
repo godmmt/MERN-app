0518e8ba53b53b2f06284fa0d498d410a43f5733
godmmt <mingreal75@gmail.com>
2023-10-03 17:49:03 +0800

    feat: 串接訂閱電子報 API

--- client/src/components/Subscription/index.jsx
+++ client/src/components/Subscription/index.jsx
@@ -1,12 +1,21 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Button from 'components/Button';
+import MailerService from 'services/mail.service';
 import './subscription.scss';
 
 const Subscription = () => {
   const emailInput = useRef(null);
+  const [subscribeMessage, setSubscribeMessage] = useState('');
 
   const handleSubscribe = async () => {
+    const email = emailInput.current.value;
     // TODO: 串接訂閱電子報 API
+    try {
+      const res = await MailerService.subscribeNewsletter(email);
+      setSubscribeMessage(res.data.message);
+    } catch (err) {
+      setSubscribeMessage(err.data.message === 'No recipients defined' ? 'Your email seems to be incorrect. Please re-enter.' : err.data.message);
+    }
   };
 
   return (
@@ -17,6 +26,7 @@ const Subscription = () => {
       <div className='action'>
         <input type='email' placeholder='Please enter your email' ref={emailInput} />
         <Button onClick={handleSubscribe}>Subscribe!</Button>
+        <p>{subscribeMessage}</p>
       </div>
     </section>
   );
--- client/src/services/mail.service.js
+++ client/src/services/mail.service.js
@@ -1,6 +1,10 @@
 // TODO：發送信件的 APIs
+
+import axios from 'config/axios.config';
 class MailerService {
-  static subscribeNewsletter(email) {}
+  static subscribeNewsletter(email) {
+    return axios.post('/mailer/subscribe-newsletter', { email });
+  }
 
   static unsubscribeNewsletter(email) {}
 
