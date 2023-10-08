23c793f8631d54d367cf9647028602df71d31353
godmmt <mingreal75@gmail.com>
2023-10-08 20:55:38 +0800

    fix: API回傳的訊息用window alert顯示

--- client/src/components/Subscription/index.jsx
+++ client/src/components/Subscription/index.jsx
@@ -1,20 +1,18 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import Button from 'components/Button';
 import MailerService from 'services/mail.service';
 import './subscription.scss';
 
 const Subscription = () => {
   const emailInput = useRef(null);
-  const [subscribeMessage, setSubscribeMessage] = useState('');
 
   const handleSubscribe = async () => {
     const email = emailInput.current.value;
-    // TODO: 串接訂閱電子報 API
     try {
       const res = await MailerService.subscribeNewsletter(email);
-      setSubscribeMessage(res.data.message);
+      window.alert(res.data.message);
     } catch (err) {
-      setSubscribeMessage(err.data.message === 'No recipients defined' ? 'Your email seems to be incorrect. Please re-enter.' : err.data.message);
+      window.alert(err.data.message === 'No recipients defined' ? 'Your email seems to be incorrect. Please re-enter.' : err.data.message);
     }
   };
 
@@ -26,7 +24,6 @@ const Subscription = () => {
       <div className='action'>
         <input type='email' placeholder='Please enter your email' ref={emailInput} />
         <Button onClick={handleSubscribe}>Subscribe!</Button>
-        <p>{subscribeMessage}</p>
       </div>
     </section>
   );
