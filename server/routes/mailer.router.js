c56cb4c6a78d179d1c923b43dce1c063b0d93772
godmmt <mingreal75@gmail.com>
2023-10-09 19:47:03 +0800

    refactor: 重置密碼驗證器併入auth驗證器

--- server/routes/mailer.router.js
+++ server/routes/mailer.router.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { MailerController } from '../controller/index.js';
-import { ResetPasswordValidator } from '../validations/index.js';
+import { AuthValidator } from '../validations/index.js';
 
 const mailerRouter = Router();
 
-mailerRouter.post('/reset-password', ResetPasswordValidator.hasEmailInfo, MailerController.resetPassword);
+mailerRouter.post('/reset-password', AuthValidator.hasEmailInfo, MailerController.resetPassword);
 mailerRouter.post('/subscribe-newsletter', MailerController.subscribeNewsLetter);
 mailerRouter.post('/unsubscribe-newsletter', MailerController.unsubscribeNewsletter);
 
--- server/validations/index.js
+++ server/validations/index.js
@@ -1,3 +1,2 @@
 export { default as AuthValidator } from './auth.validation.js';
 export { default as CourseValidator } from './course.validation.js';
-export { default as ResetPasswordValidator } from './resetPassword.validation.js';
